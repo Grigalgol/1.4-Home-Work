9a55e4fbfc10f684ab7e0ac7a2c66c2421f4e3b3
Grigoriy <grisha101202@yandex,ru>
2020-11-03 16:08:27 +0300

    ended 2

---
+++ src/Two.java
@@ -0,0 +1,8 @@
+import java.util.*;
+public class Two {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        int i =in.nextInt();
+        System.out.println(1<<i);
+    }
+}
